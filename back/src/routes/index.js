ae8f180a17c452dc410d6e99caa92bfe19a7107f
JonathanChorolque <64989037+Jonijech96@users.noreply.github.com>
2024-06-30 01:07:04 -0300

    back add: agrega uso de mercadopago desde el back

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,4 +1,6 @@
 const {Router} = require('express')
+// SDK de Mercado Pago
+const { MercadoPagoConfig,Preference } = require('mercadopago');
 const router = Router()
 
 // let pushSubscription
@@ -20,11 +22,56 @@ const router = Router()
 //     console.log("error al hacer el send notification",error);
 //   }
 // })
-
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN
+// 'TEST-5691609799867371-062316-43b4fdc7fc1adf6bfafd657758a7ad6d-144774982'
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+// 'TEST-af83b07b-9802-49f9-84bc-0ef8a80ff4d1'
 router.get('/api/log', (req, res) => {
   console.log("¡Ruta GET /log accedida!");
   res.status(200).json({ message: "Mensaje registrado en la consola" });
 });
+
+
+
+
+router.post('/api/create_preference', async (req,res)=>{
+  try {
+    // Agrega credenciales
+    const client = new MercadoPagoConfig({ accessToken: ACCESS_TOKEN });
+const preference = new Preference(client);
+const { cart } = JSON.parse(Object.keys(req.body)[0]);;
+    const body = {
+      items: 
+      cart.map(item => ({
+        title: item.title,
+        quantity:Number(item.quantity),
+        unit_price:Number(item.price),
+        currency_id: "ARS",
+      })),
+      // [
+      //   {
+      //     title: req.body.title,
+      //     quantity: Number(req.body.quantity),
+      //     unit_price: Number(req.body.price),
+      //     currency_id: "ARS",
+      //   }
+      // ],
+      back_urls: {
+        success: "https://www.youtube.com/watch?v=-VD-l5BQsuE",
+        failure: "https://www.youtube.com/watch?v=-VD-l5BQsuE",
+        pending: "https://www.youtube.com/watch?v=-VD-l5BQsuE"
+      },
+      auto_return: "approved",
+    };
+    const result = await preference.create({ body }).catch(e=>console.log(e));
+    res.json({ id: result.id })
+  } catch (error) {
+    res.status(500).json({error: 'error al crear la preferencia'})
+    console.log("error al hacer el send notification",error);
+  }
+})
+
+
 
 module.exports = router;
 
